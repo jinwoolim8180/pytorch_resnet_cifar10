87465ce8a0e5a2f8848fdb37d062e71c3ef48437
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-09-02 01:21:44 +0900

    update

---
+++ mobiconv.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MobiConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, groups=1, padding=1, stride=1, bias=True,
+                 n_pools=3, n_layers=16, n_pruned=0, ratio=[1, 0]):
+        super(MobiConvBlock, self).__init__()
+        # out_channels should be divisible by n_layers
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.n_pools = n_pools
+        self.n_layers = n_layers
+        self.n_pruned = n_pruned
+        self.ratio = ratio
+        assert out_channels >= n_pools * n_layers
+
+        self.convs = nn.ModuleList()
+        if groups != 1:
+            for i in range(n_pools):
+                self.convs.append(
+                    nn.Conv2d(in_channels, n_layers, kernel_size=kernel_size, groups=n_layers,
+                              padding=padding, stride=stride, bias=bias)
+                )
+            self.convs.append(
+                nn.Conv2d(in_channels, out_channels - n_pools * n_layers, kernel_size=kernel_size,
+                          groups=out_channels - n_pools * n_layers,
+                          padding=padding, stride=stride, bias=bias)
+            )
+        else:
+            for i in range(n_pools):
+                self.convs.append(
+                    nn.Conv2d(in_channels, n_layers, kernel_size=kernel_size,
+                              padding=padding, stride=stride, bias=bias)
+                )
+            self.convs.append(
+                nn.Conv2d(in_channels, out_channels - n_pools * n_layers, kernel_size=kernel_size,
+                          padding=padding, stride=stride, bias=bias)
+            )
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        size = 2 ** self.n_pools
+        out = []
+        table = torch.ones(N, 1, H // self.stride, W // self.stride).cuda()
+        for conv in self.convs:
+            h = F.max_pool2d(x, kernel_size=size, stride=size)
+            h = conv(h)
+            h = F.upsample(h, scale_factor=size, mode='nearest')
+            h *= table
+            threshold = self.ratio[0] * torch.mean(h, dim=(-2, -1), keepdim=True)
+            threshold += self.ratio[1] * torch.amin(h, dim=(-2, -1), keepdim=True)
+            threshold /= self.ratio[0] + self.ratio[1]
+            table = torch.sum(torch.ge(h, threshold).float(), dim=1, keepdim=True)
+            table += self.n_pruned * torch.ones(N, 1, H // self.stride, W // self.stride).cuda()
+            out.append(h)
+            size //= 2
+        out = torch.cat(out, dim=1)
+        return out
+
+
+class SmartPool2d(nn.Module):
+    def __init__(self, scale, mode='avgpool', ratio=0.1):
+        super(SmartPool2d, self).__init__()
+        self.scale = scale
+        self.mode = mode
+        self.ratio = ratio
+
+    def _crop(self, x):
+        N, C, H, W = x.shape
+        threshold = self.ratio * torch.amax(x, dim=(-2, -1))
+        table = torch.ge(x, threshold.unsqueeze(2).unsqueeze(3))
+        x_range = torch.tile(torch.arange(H), (N, C, W, 1)).permute(0, 1, 3, 2).cuda()
+        y_range = torch.tile(torch.arange(W), (N, C, H, 1)).cuda()
+        x_min = torch.amin(torch.logical_not(x_range) * 1e5 + table * x_range, dim=(-2, -1))
+        x_max = torch.amax(torch.logical_not(x_range) * -1e5 + table * x_range, dim=(-2, -1))
+        y_min = torch.amin(torch.logical_not(y_range) * 1e5 + table * y_range, dim=(-2, -1))
+        y_max = torch.amax(torch.logical_not(y_range) * -1e5 + table * y_range, dim=(-2, -1))
+        out = []
+        for n in range(N):
+            stack = []
+            for c in range(C):
+                feature = x[n, c, int(x_min[n, c].item()):int(x_max[n, c].item()) + 1,
+                            int(y_min[n, c].item()):int(y_max[n, c].item()) + 1]
+                print(feature.shape)
+                stack.append(feature)
+            out.append(torch.stack(stack, dim=1))
+        return torch.stack(out, dim=0)
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        threshold = self.ratio * torch.amax(x, dim=(-2, -1))
+        table = torch.ge(x, threshold.unsqueeze(2).unsqueeze(3))
+        x_range = torch.tile(torch.arange(H), (N, C, W, 1)).permute(0, 1, 3, 2).cuda()
+        y_range = torch.tile(torch.arange(W), (N, C, H, 1)).cuda()
+        x_min = torch.amin(torch.logical_not(x_range) * 1e5 + table * x_range, dim=(-2, -1))
+        x_max = torch.amax(torch.logical_not(x_range) * -1e5 + table * x_range, dim=(-2, -1))
+        y_min = torch.amin(torch.logical_not(y_range) * 1e5 + table * y_range, dim=(-2, -1))
+        y_max = torch.amax(torch.logical_not(y_range) * -1e5 + table * y_range, dim=(-2, -1))
+        out = []
+        for n in range(N):
+            stack = []
+            for c in range(C):
+                feature = x[n, c, int(x_min[n, c].item()):int(x_max[n, c].item()) + 1,
+                          int(y_min[n, c].item()):int(y_max[n, c].item()) + 1]
+                if self.mode == 'avgpool':
+                    feature = F.interpolate(feature.unsqueeze(0).unsqueeze(1), size=(H // self.scale, W // self.scale),
+                                            align_corners=False, antialias=True, mode='bilinear')
+                elif self.mode == 'maxpool':
+                    feature = F.interpolate(feature.unsqueeze(0).unsqueeze(1), size=(H, W),
+                                            align_corners=False, antialias=True, mode='bilinear')
+                    feature = F.max_pool2d(feature, kernel_size=self.scale, stride=self.scale)
+                feature = feature.squeeze(1).squeeze(0)
+                stack.append(feature)
+            out.append(torch.stack(stack, dim=0))
+        out = torch.stack(out, dim=0)
+        return out
--- resnet.py
+++ resnet.py
@@ -33,6 +33,7 @@ import torch.nn.functional as F
 import torch.nn.init as init
 
 from torch.autograd import Variable
+from mobiconv import MobiConvBlock
 
 __all__ = ['ResNet', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202']
 
@@ -54,11 +55,13 @@ class LambdaLayer(nn.Module):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1, option='A'):
+    def __init__(self, in_planes, planes, stride=1, n_layers=2, n_pools=0, option='A'):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = MobiConvBlock(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False,
+                                   n_layers=n_layers, n_pools=n_pools)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = MobiConvBlock(planes, planes, kernel_size=3, stride=1, padding=1, bias=False,
+                                   n_layers=n_layers, n_pools=n_pools)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
@@ -90,18 +93,18 @@ class ResNet(nn.Module):
 
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
-        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1, n_layers=2, n_pools=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2, n_layers=2, n_pools=1)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2, n_layers=2, n_pools=0)
         self.linear = nn.Linear(64, num_classes)
 
         self.apply(_weights_init)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride, n_layers, n_pools):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, n_layers=n_layers, n_pools=n_pools))
             self.in_planes = planes * block.expansion
 
         return nn.Sequential(*layers)
